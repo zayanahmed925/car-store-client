14b989ae2291b3411ef5315924342a595d0977e8
zayanahmed925 <rjraju925@gmail.com>
2022-05-07 16:32:48 +0600

    Singel item delete done

--- src/page/manageItems/ManageItems.js
+++ src/page/manageItems/ManageItems.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import useItems from '../Hooks/useItems';
-import ManageItem from './ManageItem';
 
 const ManageItems = () => {
     const [items, setItems] = useItems()
-    // console.log(items);
+    const handleDelete = (id) => {
+        console.log(id);
+        const proceed = window.confirm('Are you sure want to Delete?');
+        if (proceed) {
+            const url = `http://localhost:5000/product/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const remaining = items.filter(item => item._id !== id);
+                    setItems(remaining);
+
+                })
+        }
+    }
     return (
         <div>
             <div className='row'>
                 {
-                    items?.map(item => <ManageItem key={items._id}
-                        item={item}
-                    ></ManageItem>)
+                    items?.map(item => <div className="gy-3 col-sm-12 col-md-6 col-lg-4">
+                        <div className="card h-100">
+                            <img src={item.img} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">{item.name}</h5>
+                                <p>{item.price}</p>
+                                <p>Quantity: <small>{item.quantity}</small></p>
+                                <p>Supplier: {item.supplier}</p>
+                                <p className="card-text">{item.description}</p>
+                                <button className='btn btn-danger' onClick={() => handleDelete(item._id)} >Delete</button>
+
+                            </div>
+                        </div>
+
+                    </div>
+                    )
                 }
 
             </div>
